62b14f0a39f565b396e0e226e46a255f19a319e1
Victor Henrique <victor-silva02@outlook.com>
2022-09-15 22:16:26 -0300

    Create index.js

---
+++ src/components/HomeContent/index.js
@@ -0,0 +1,14 @@
+
+import { Home, Informations, Text, GetStarted } from "./style";
+
+export default function HomeContent() {
+  return (
+    <Home>
+      <Informations>
+        <h1>More than just shorter links</h1>
+        <Text>Build your brand's recognition and get detailed insights on how your links are performing.</Text>
+        <GetStarted>Get Started</GetStarted>
+      </Informations>
+    </Home>
+  );
+}
