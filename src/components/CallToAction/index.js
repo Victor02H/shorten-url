b3d524a9be2ae4394e4f5f88ec18125940ed8d3a
Victor Henrique <victor-silva02@outlook.com>
2022-09-15 22:16:05 -0300

    Create index.js

---
+++ src/components/CallToAction/index.js
@@ -0,0 +1,10 @@
+import { BoostContainer, GetStarted } from './style';
+
+export default function CallToAction() {
+  return (
+    <BoostContainer>
+      <h2>Boost your links today</h2>
+      <GetStarted>Get Started</GetStarted>
+    </BoostContainer>
+  );
+}
